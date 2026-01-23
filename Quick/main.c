2139303fbbd45338b3175a20ff8128bb2a64b924
98andreea <andreea.rosu@student.upt.ro>
2026-01-23 02:00:56 +0200

    Lab 6 is done

--- Quick/main.c
+++ Quick/main.c
@@ -1,27 +1,27 @@
-//Andreea Rosu
-//Informatica
-//Anul III, 2025
-#include"utils.h"
-#include"lexer.h"
-#include"parser.h"
-
-#include<stdio.h>
-
-int main() {
-	char* inbuf = loadFile("test/1.q");
-	puts(inbuf);
-	printf("\n");
-	printf("~~~~ANALIZATORUL LEXAICAL~~~~\n");
-	printf("Atomii sunt: \n");
-	tokenize(inbuf);
-	showTokens();
-	printf("\n");
-
-	printf("~~~~ANALIZATORUL SINTACTIC SI ANALIZA DE DOMENIU~~~~\n");
-	parse();
-	printf("Program sintactic corect\n");
-	printf("\n");
-	printf("\n");
-
-	return 0;
-}
+////Andreea Rosu
+////Informatica
+////Anul III, 2025
+//#include"utils.h"
+//#include"lexer.h"
+//#include"parser.h"
+//
+//#include<stdio.h>
+//
+//int main() {
+//	char* inbuf = loadFile("test/1.q");
+//	puts(inbuf);
+//	printf("\n");
+//	printf("~~~~ANALIZATORUL LEXAICAL~~~~\n");
+//	printf("Atomii sunt: \n");
+//	tokenize(inbuf);
+//	showTokens();
+//	printf("\n");
+//
+//	printf("~~~~ANALIZATORUL SINTACTIC SI ANALIZA DE DOMENIU~~~~\n");
+//	parse();
+//	printf("Program sintactic corect\n");
+//	printf("\n");
+//	printf("\n");
+//
+//	return 0;
+//}
--- Quick/parser.c
+++ Quick/parser.c
@@ -11,6 +11,9 @@
 #include "lexer.h"
 #include "ad.h"
 
+//lab 6
+#include "gen.h"
+
 Ret ret;
 Symbol* crtFn = NULL;
 
@@ -49,7 +52,7 @@ bool consume(int code) {
     return false;
 }
 
-////+
+// lab 5
 //void setRet(int type, bool lval) {
 //    ret.type = type;
 //    ret.lval = lval;
@@ -77,6 +80,8 @@ bool defVar() {
     int star = iTk;
     Symbol* s = NULL;
 
+    const char* name = NULL;
+
     if (!consume(VAR)) {
         iTk = star;
         return false;
@@ -87,7 +92,7 @@ bool defVar() {
         }
 
         {
-            const char* name = consumed->text;
+            name = consumed->text;
             s = searchInCurrentDomain(name);
             if (s) tkerr("symbol redefinition: %s", name);
             s = addSymbol(name, KIND_VAR);
@@ -109,6 +114,10 @@ bool defVar() {
             tkerr("Eroare: Lipseste ';' dupa numele variabilei");
         }
     }
+
+    //lab 6
+    Text_write(crtVar, "%s %s;\n", cType(ret.type), name);
+
     return true;
 }
 
@@ -125,6 +134,13 @@ bool defFunc() {
         }
 
         const char* name = consumed->text;
+
+        //lab 6
+        crtCode = &tFunctions;
+        crtVar = &tFunctions;
+        Text_clear(&tFnHeader);
+        Text_write(&tFnHeader, "%s(", name);
+
         Symbol* s = searchInCurrentDomain(name);
         if (s) tkerr("Simbol redefinit: %s", name);
         crtFn = addSymbol(name, KIND_FN);
@@ -147,6 +163,9 @@ bool defFunc() {
 
         crtFn->type = ret.type;
 
+        //lab 6
+        Text_write(&tFunctions, "\n%s %s){\n", cType(ret.type), tFnHeader.buf);
+
         while (defVar()) {}
         if (!block()) {
             tkerr("Eroare: Blocul functiei lipseste");
@@ -156,6 +175,11 @@ bool defFunc() {
         }
     }
 
+    //lab 6
+    Text_write(&tFunctions, "}\n");
+    crtCode = &tMain;
+
+    crtVar = &tBegin;
     delDomain();
     crtFn = NULL;
     return true;
@@ -179,6 +203,9 @@ bool funcParams() {
     }
     while (consume(COMMA)) {
         if (!funcParam()) {
+            //lab 6
+            Text_write(&tFnHeader, ", ");
+
             tkerr("Eroare: Lipseste parametru dupa ','");
         }
     }
@@ -208,6 +235,10 @@ bool funcParam() {
     s->type = ret.type;
     sFnParam->type = ret.type;
 
+
+    //lab 6
+    Text_write(&tFnHeader, "%s %s", cType(ret.type), name);
+
     return true;
 }
 
@@ -215,47 +246,86 @@ bool instr() {
     int star = iTk;
 
     if (consume(IF)) {
+        //lab 6
+        Text_write(crtCode, "if(");
+
         if (!consume(LPAR)) tkerr("Eroare: Lipseste '(' dupa IF");
         if (!expr()) tkerr("Eroare: Lipseste expresia dupa IF");
 
-        //+
+        // lab 5
         if (ret.type == TYPE_STR) tkerr("the if condition must have type int or real");
 
         if (!consume(RPAR)) tkerr("Eroare: Lipseste ')' dupa IF");
+        
+        //lab 6
+        Text_write(crtCode, "){\n");
+
         if (!block()) tkerr("Eroare: Lipseste blocul dupa IF");
+
+        //lab 6
+        Text_write(crtCode, ")}\n");
+
         if (consume(ELSE)) {
+            //lab 6
+            Text_write(crtCode, "){\n");
+
             if (!block()) tkerr("Eroare: Lipseste blocul dupa ELSE");
+
+            //lab 6
+            Text_write(crtCode, ")}\n");
         }
         if (!consume(END)) tkerr("Eroare: Lipseste END dupa IF");
         return true;
     }
 
     if (consume(RETURN)) {
+        //lab 6
+        Text_write(crtCode, "return ");
+
         if (!expr()) tkerr("Eroare: Lipseste expresie dupa RETURN");
 
-        //+
+        // lab 5
         if (!crtFn) tkerr("return can be used only in a function");
         if (ret.type != crtFn->type) tkerr("the return type must be the same as function return type");
 
         if (!consume(SEMICOLON)) tkerr("Eroare: Lipseste ';' dupa RETURN");
+
+        //lab 6 
+        Text_write(crtCode, ";\n");
+
         return true;
     }
 
     if (consume(WHILE)) {
+        //lab 6
+        Text_write(crtCode, "while(");
+
         if (!consume(LPAR)) tkerr("Eroare: Lipseste '(' dupa WHILE");
         if (!expr()) tkerr("Eroare: Lipseste expresia dupa WHILE");
 
-        //+
+        // lab 5
         if (ret.type == TYPE_STR) tkerr("the while condition must have type int or real");
 
         if (!consume(RPAR)) tkerr("Eroare: Lipseste ')' dupa WHILE");
+
+        //lab 6
+        Text_write(crtCode, "){\n");
+
         if (!block()) tkerr("Eroare: Lipseste blocul dupa WHILE");
         if (!consume(END)) tkerr("Eroare: Lipseste 'END' dupa WHILE");
+
+        //lab 6
+        Text_write(crtCode, ")}\n");
+
         return true;
     }
 
     if (expr()) {
         if (!consume(SEMICOLON)) tkerr("Eroare: Lipseste ';' dupa expresie");
+
+        //lab 6
+        Text_write(crtCode, ";\n");
+
         return true;
     }
 
@@ -271,16 +341,20 @@ bool exprLogic() {
     if (!exprAssign()) return false;
     while (1) {
 
-        //+
+        // lab 5
         Ret leftType = ret;
 
         if (consume(AND) || consume(OR)) {
-            //+
+            //lab 6
+            int op = consumed->code;
+            Text_write(crtCode, (op == AND) ? " && " : " || ");
+
+            //lab 5
             if (leftType.type == TYPE_STR) tkerr("the left operand of && or || cannot be str");
 
             if (!exprAssign()) tkerr("Lipseste expresia dupa operatorul logic");
 
-            //+
+            // lab 5
             if (ret.type == TYPE_STR) tkerr("the right operand of && or || cannot be str");
             setRet(TYPE_INT, false);
         }
@@ -294,11 +368,15 @@ bool exprAssign() {
     if (consume(ID)) {
         const char* name = consumed->text;
         if (consume(ASSIGN)) {
+
+            //lab 6
+            Text_write(crtCode, "%s = ", name);
+
             if (!exprComp()) {
                 tkerr("Eroare: Lipseste expresia dupa '='");
             }
 
-            //+
+            // lab 5
             Symbol* s = searchSymbol(name);
             if (!s) tkerr("undefined symbol: %s", name);
             if (s->kind == KIND_FN) tkerr("a function cannot be used as destination for assignment");
@@ -315,12 +393,16 @@ bool exprAssign() {
 bool exprComp() {
     if (!exprAdd()) return false;
     if (consume(LESS) || consume(EQUAL)) {
-        //+
+        //lab 6
+        int op = consumed->code;
+        Text_write(crtCode, (op == LESS) ? " < " : " == ");
+
+        // lab 5
         Ret leftType = ret;
 
         if (!exprAdd()) tkerr("Lipseste operandul dupa operatorul de comparatie");
 
-        //+
+        // lab 5
         if (leftType.type != ret.type) tkerr("different types for comparison");
         setRet(TYPE_INT, false);
     }
@@ -331,13 +413,17 @@ bool exprAdd() {
     if (!exprMul()) return false;
     while (1) {
         if (consume(ADD) || consume(SUB)) {
-            //+
+            //lab 6
+            int op = consumed->code;
+            Text_write(crtCode, (op == ADD) ? " + " : " - ");
+
+            // lab 5
             Ret leftType = ret;
             if (leftType.type == TYPE_STR) tkerr("operands of + or - cannot be str");
 
             if (!exprMul()) tkerr("Lipseste operandul dupa '+' sau '-'");
 
-            //+
+            // lab 5
             if (leftType.type != ret.type) tkerr("different types for + or -");
             setRet(leftType.type, false);
         }
@@ -350,13 +436,17 @@ bool exprMul() {
     if (!exprPrefix()) return false;
     while (1) {
         if (consume(MUL) || consume(DIV)) {
-            //+
+            //lab 6
+            int op = consumed->code;
+            Text_write(crtCode, (op == MUL) ? " * " : " / ");
+
+            // lab 5
             Ret leftType = ret;
             if (leftType.type == TYPE_STR) tkerr("operands of * or / cannot be str");
 
             if (!exprPrefix()) tkerr("Lipseste operandul dupa '*' sau '/'");
 
-            //+
+            // lab 5
             if (leftType.type != ret.type) tkerr("different types for * or /");
             setRet(leftType.type, false);
         }
@@ -367,18 +457,24 @@ bool exprMul() {
 
 bool exprPrefix() {
     if (consume(SUB)) {
+        //lab 6
+        Text_write(crtCode, "-");
+
         if (!factor()) tkerr("Factor invalid");
 
-        //+
+        // lab 5
         if (ret.type == TYPE_STR) tkerr("unary - must be int or real");
         setRet(ret.type, false);
 
         return true;
     }
     if (consume(NOT)) {
+        //lab 6
+        Text_write(crtCode, "!");
+
         if (!factor()) tkerr("Factor invalid");
 
-        //+
+        // lab 5
         if (ret.type == TYPE_STR) tkerr("! must be int or real");
         setRet(TYPE_INT, false);
 
@@ -389,52 +485,92 @@ bool exprPrefix() {
 
 bool factor() {
     int star = iTk;
-    //+
-    if (consume(INT)) { setRet(TYPE_INT, false); return true; }
-    if (consume(REAL)) { setRet(TYPE_REAL, false); return true; }
-    if (consume(STR)) { setRet(TYPE_STR, false); return true; }
+    // lab 5
+    if (consume(INT)) {
+        //lab 6
+        Text_write(crtCode, "%d", consumed->i);
+
+        setRet(TYPE_INT, false); 
+        return true; 
+    }
+    if (consume(REAL)) { 
+        //lab 6
+        Text_write(crtCode, "%g", consumed->r);
+
+        setRet(TYPE_REAL, false); 
+        return true; 
+    }
+    if (consume(STR)) { 
+        //lab 6
+        Text_write(crtCode, "\"%s\"", consumed->text);
+
+        setRet(TYPE_STR, false); 
+        return true; 
+    }
 
     if (consume(LPAR)) {
+        //lab 6
+        Text_write(crtCode, "(");
+
         if (!expr()) tkerr("Expresie lipsa dupa '('");
         if (!consume(RPAR)) tkerr("Lipseste ')'");
+
+        //lab 6
+        Text_write(crtCode, ")");
+
         return true;
     }
 
     if (consume(ID)) {
-        //+
+        //lab 6
+        const char* name = consumed->text;
+
+        // lab 5
         Symbol* s = searchSymbol(consumed->text);
         if (!s) tkerr("undefined symbol: %s", consumed->text);
 
+        //lab 6
+        Text_write(crtCode, "%s", name);
+
         if (consume(LPAR)) {
-            //+
+            //lab 6
+            Text_write(crtCode, "(");
+
+            // lab 5
             if (s->kind != KIND_FN) tkerr("%s is not a function", s->name);
             Symbol* argDef = s->args;
 
             if (expr()) {
-                //+
+                // lab 5
                 if (!argDef) tkerr("too many arguments for %s", s->name);
                 if (argDef->type != ret.type) tkerr("argument type mismatch");
                 argDef = argDef->next;
 
                 while (consume(COMMA)) {
+                    //lab 6
+                    Text_write(crtCode, ", ");
+
                     if (!expr()) tkerr("Lipseste expresie dupa ','");
 
-                    //+
+                    // lab 5
                     if (!argDef) tkerr("too many arguments");
                     if (argDef->type != ret.type) tkerr("argument type mismatch");
                     argDef = argDef->next;
                 }
             }
 
-            //+
+            // lab 5
             if (argDef) tkerr("too few arguments for %s", s->name);
 
             if (!consume(RPAR)) tkerr("Lipseste ')' la apelul functiei");
 
-            //+
+            //lab 6
+            Text_write(crtCode, ")");
+
+            // lab 5
             setRet(s->type, false);
         }
-        //+
+        // lab 5
         else {
             if (s->kind == KIND_FN) tkerr("function %s can only be called", s->name);
             setRet(s->type, true);
@@ -447,9 +583,15 @@ bool factor() {
 }
 
 bool program() {
+    //lab 6
+    crtCode = &tMain;
+    crtVar = &tBegin;
+    Text_write(&tBegin, "#include \"quick.h\"\n\n");
+    Text_write(&tMain, "\nint main(){\n");
+
     addDomain();
 
-    //+
+    // lab 5
     addPredefinedFns();
 
     while (1) {
